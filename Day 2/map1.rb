ec7df690d2e02d69b287566b0e2566bd3bed9ca0
Roshan <roshan.shrestha@lftechnology.com>
2014-12-30 21:29:28 +0545

    Code review

--- Day 2/map1.rb
+++ Day 2/map1.rb
@@ -1,10 +1,10 @@
 def custom_map(array)
-  arr=[]
+  arr = []
   for i in 0..array.length-1
-    arr[i]=yield array[i]
+    arr[i] = yield array[i]
   end
   return arr
 end
-mappedArray=custom_map([1,2,3,4,5]){|x|x*2*x}
+mappedArray = custom_map([1,2,3,4,5]){|x|x*2*x}
 print mappedArray
 
